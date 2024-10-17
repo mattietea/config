7457fbdb1e79876a72ec59424d71d322fc138f45
mattietea <mattcthomas@me.com>
2024-10-17 23:45:55 +0100

    Update

---
+++ modules/packages/pure.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+
+  programs.zsh = {
+    initExtra = ''
+      zstyle :prompt:pure:git:branch color yellow
+
+      autoload -U promptinit; promptinit
+      prompt pure
+    '';
+
+    plugins = [
+      {
+        name = "pure";
+        src = "${pkgs.pure-prompt}/share/zsh/site-functions";
+      }
+    ];
+  };
+
+}
