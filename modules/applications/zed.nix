20cf1f551f40ad75bbae1d38debcfa76600e6b5b
mattietea <mattcthomas@me.com>
2025-09-20 11:11:29 +0100

    setup nix

--- modules/applications/zed.nix
+++ modules/applications/zed.nix
@@ -1,12 +1,49 @@
-{ pkgs
-, ...
+{
+  pkgs,
+  ...
 }:
 
 {
 
+  home.packages = with pkgs; [
+    nixfmt
+    nixd
+  ];
+
   # https://home-manager-options.extranix.com/?query=programs.zed&release=master
 
-  programs.zed-editor.enable = true;
+  programs.zed-editor = {
+    enable = true;
+
+    extensions = [
+      "nix"
+    ];
+
+    userSettings = {
+
+      languages = {
+        Nix = {
+          language_servers = [
+            "nixd"
+          ];
+        };
+      };
+
+      lsp = {
+        nixd = {
+          binary = {
+            path = "${pkgs.nixd}/bin/nixd";
+          };
+          settings = {
+            diagnostic = {
+              suppress = [ "sema-extra-with" ];
+            };
+          };
+        };
+      };
+    };
+
+  };
 
   # programs.zed-editor.userKeymaps = [
   #   {
