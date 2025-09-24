e994e26c7a9df2ff152f0d28829669b921b133ae
Matt Thomas <matt.thomas@shopify.com>
2025-09-24 08:46:26 +0100

    Update

--- modules/packages/television.nix
+++ modules/packages/television.nix
@@ -23,26 +23,10 @@
     };
 
     channels = {
-      git-log = {
-        metadata = {
-          description = "A channel to select from git log entries";
-          name = "git-log";
-          requirements = [
-            "git"
-          ];
-        };
-        preview = {
-          command = "git show -p --stat --pretty=fuller --color=always '{0}'";
-        };
-        source = {
-          command = "git log --oneline --date=short --pretty=\"format:%h %s %an %cd\" \"$@\"";
-          output = "{split: :0}";
-        };
-      };
       files = {
         metadata = {
           description = "A channel to select files and directories";
-          name = "file";
+          name = "files";
           requirements = [
             "fd"
             "bat"
@@ -53,7 +37,7 @@
         };
         source = {
           command = [
-            "${pkgs.fd}/bin/fd -t f"
+            # "${pkgs.fd}/bin/fd -t f"
             "${pkgs.fd}/bin/fd -t f -H"
           ];
         };
@@ -76,34 +60,6 @@
           ];
         };
       };
-      text = {
-        metadata = {
-          description = "A channel to find and select text from files";
-          name = "text";
-          requirements = [
-            "rg"
-            "bat"
-          ];
-        };
-        ui = {
-          preview_panel = {
-            header = "{split:\\::..2}";
-          };
-        };
-        preview = {
-          command = "${pkgs.bat}/bin/bat -n --color=always '{split:\\::0}'";
-          offset = "{split:\\::1}";
-          env = {
-            BAT_THEME = "ansi";
-          };
-        };
-        source = {
-          command = "${pkgs.ripgrep}/bin/rg . --no-heading --line-number";
-          display = "[{split:\\::..2}]\t{split:\\::2..}";
-          output = "{split:\\::..2}";
-        };
-      };
-
     };
   };
 
