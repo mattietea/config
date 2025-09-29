ab090a7988dfca15c727a811399b49bd56658671
mattietea <mattcthomas@me.com>
2025-09-29 21:04:09 +0100

    updates

--- modules/packages/git-absorb.nix
+++ modules/packages/git-absorb.nix
@@ -9,13 +9,14 @@
     # https://github.com/tummychow/git-absorb?tab=readme-ov-file#configuration
 
     absorb = {
-      maxStack = 30;
+      maxStack = 40;
       oneFixupPerCommit = true;
       autoStageIfNothingStaged = true;
+      forceAuthor = true;
     };
   };
 
   programs.git.aliases = {
-    soak = "!git absorb --and-rebase --base $(git merge-base HEAD origin/main)";
+    soak = "!git absorb --and-rebase";
   };
 }
