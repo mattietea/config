43516df016767e37d881274854655dd17789445b
mattietea <mattcthomas@me.com>
2025-10-19 19:55:27 +0100

    Change package channel to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "mattietea's system configuration file";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
