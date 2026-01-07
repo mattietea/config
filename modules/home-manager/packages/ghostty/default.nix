b608b8af77c0196dcc8cb8c5694e830521758942
Matt Thomas <matt.thomas@shopify.com>
2026-01-07 12:17:42 +0700

    Ghostty fontweight

--- modules/home-manager/packages/ghostty/default.nix
+++ modules/home-manager/packages/ghostty/default.nix
@@ -7,6 +7,8 @@
     settings = {
       font-family = "JetBrainsMono NFM Bold";
       font-size = 14;
+      font-thicken = true;
+      font-thicken-strength = 255;
       theme = "light:GitHub Light High Contrast,dark:GitHub Dark High Contrast";
       window-decoration = "auto";
       macos-titlebar-style = "tabs";
