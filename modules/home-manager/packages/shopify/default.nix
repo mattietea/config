658a3385dae44f586c04c3db05870a1c4cef3a27
Matt Thomas <matt.thomas@shopify.com>
2026-01-07 12:19:58 +0700

    Fix Shopify

--- modules/home-manager/packages/shopify/default.nix
+++ modules/home-manager/packages/shopify/default.nix
@@ -1,7 +1,7 @@
 _: {
   programs.zsh.initContent = ''
     [[ -f /opt/dev/dev.sh ]] && source /opt/dev/dev.sh
-    [[ -x /usr/local/bin/brew ]] && eval $(/usr/local/bin/brew shellenv)
-    [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+    [[ -x /usr/local/bin/brew ]] && eval "$(/usr/local/bin/brew shellenv)"
+    [[ -x /opt/homebrew/bin/brew ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
   '';
 }
