120a3839ed08fcab57234be82fface67e598996d
mattietea <mattcthomas@me.com>
2026-01-20 17:36:31 +0000

    build: migrate to flake-parts
    - Add flake-parts input with nixpkgs-lib follows
    - Restructure outputs using flake-parts.lib.mkFlake
    - Use perSystem for formatter definition
    - Maintain same darwinConfigurations outputs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
-  description = "mattietea's system configuration file";
+  description = "mattietea's system configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,22 +20,24 @@
   };
 
   outputs =
-    {
-      nixpkgs,
-      ...
-    }@inputs:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "aarch64-darwin"
         "x86_64-darwin"
       ];
-    in
-    {
-      darwinConfigurations = {
-        Matts-Work-MacBook-Pro = import ./hosts/work { inherit inputs; };
-        Matts-Personal-Macbook-Air = import ./hosts/personal { inherit inputs; };
+
+      flake = {
+        darwinConfigurations = {
+          Matts-Work-MacBook-Pro = import ./hosts/work { inherit inputs; };
+          Matts-Personal-Macbook-Air = import ./hosts/personal { inherit inputs; };
+        };
       };
 
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt;
+        };
     };
 }
