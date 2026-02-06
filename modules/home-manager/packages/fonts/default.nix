48d15d9a1bcecfa0b12a6ac927acbcaf2149dc5a
mattietea <mattcthomas@me.com>
2026-02-06 16:18:31 +0000

    chore: trim fonts to JetBrains Mono only
    Remove unused Nerd Font families (Geist Mono, Meslo, Hack, Iosevka).
    Only JetBrains Mono is referenced by ghostty and zed.

--- modules/home-manager/packages/fonts/default.nix
+++ modules/home-manager/packages/fonts/default.nix
@@ -4,11 +4,7 @@
 }:
 {
   home.packages = with pkgs; [
-    nerd-fonts.geist-mono
     nerd-fonts.jetbrains-mono
-    nerd-fonts.meslo-lg
-    nerd-fonts.hack
-    nerd-fonts.iosevka
   ];
 
   fonts.fontconfig.enable = true;
