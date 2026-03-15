bb560b2b26f0bf9c51199a13aa1c70c729dd4815
Matt Thomas <mattcthomas@me.com>
2026-03-15 19:34:59 -0400

    Add declarative nix daemon settings for faster evaluation (#41)
    Enable multi-core evaluation (eval-cores = 0) and add nix-community
    binary cache via Determinate Nix's nix.custom.conf override. Reduces
    darwin config evaluation from ~21s to ~6s.

---
+++ modules/darwin/system/nix.nix
@@ -0,0 +1,14 @@
+{
+  ##########################################################################
+  # Nix daemon settings (Determinate Nix override)
+  #
+  # Determinate Nix owns /etc/nix/nix.conf and includes nix.custom.conf
+  # for user overrides. We manage that file here.
+  ##########################################################################
+
+  environment.etc."nix/nix.custom.conf".text = ''
+    eval-cores = 0
+    extra-substituters = https://devenv.cachix.org https://nix-community.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+  '';
+}
