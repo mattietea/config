36ef1bdcc595f3f67c1b6706d96c77d33ade3957
Matt Thomas <mattcthomas@me.com>
2026-03-18 07:08:49 -0400

    Add opencode and claude-code-nix cachix binary caches

--- modules/darwin/system/nix.nix
+++ modules/darwin/system/nix.nix
@@ -8,7 +8,7 @@
 
   environment.etc."nix/nix.custom.conf".text = ''
     eval-cores = 0
-    extra-substituters = https://devenv.cachix.org https://nix-community.cachix.org
-    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+    extra-substituters = https://devenv.cachix.org https://nix-community.cachix.org https://opencode.cachix.org https://claude-code-nix.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= opencode.cachix.org-1:LdhuFTs/xrlYuchvsF+cOBCgCKEJIcesw9ef06GPlXU= claude-code-nix.cachix.org-1:VzA1HW3CkJnuSQaPE1t7OfSaleacUnO19VrZ3hJFH+0=
   '';
 }
