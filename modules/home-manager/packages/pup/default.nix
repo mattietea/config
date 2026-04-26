505166a0dd65c25d4e4fe7feebfd613c20dabf13
mattietea <100300487+mattietea@users.noreply.github.com>
2026-04-26 01:21:03 +0000

    Update dependencies

--- modules/home-manager/packages/pup/default.nix
+++ modules/home-manager/packages/pup/default.nix
@@ -3,7 +3,7 @@
   ...
 }:
 let
-  version = "0.53.2";
+  version = "0.54.0";
 
   pup = pkgs.stdenv.mkDerivation {
     pname = "pup";
@@ -11,7 +11,7 @@ let
 
     src = pkgs.fetchurl {
       url = "https://github.com/datadog-labs/pup/releases/download/v${version}/pup_${version}_Darwin_arm64.tar.gz";
-      hash = "sha256-QgAvwsJEC6zNMad02ui0vmGt2zzgRftxpWvEn/M8rrw=";
+      hash = "sha256-ulaH7Bck4HegytrjCVlvUIi/hej9ug3fVv3fC2cgTE0=";
     };
 
     sourceRoot = ".";
