de79793c5f55a2ea112cba45140511a6bf4c0a87
Matt Thomas <mattcthomas@me.com>
2026-05-28 21:55:38 -0400

    Update dependencies (#145)
    Automated dependency update.

--- modules/home-manager/packages/pup/default.nix
+++ modules/home-manager/packages/pup/default.nix
@@ -3,7 +3,7 @@
   ...
 }:
 let
-  version = "0.64.1";
+  version = "0.64.2";
 
   pup = pkgs.stdenv.mkDerivation {
     pname = "pup";
@@ -11,7 +11,7 @@ let
 
     src = pkgs.fetchurl {
       url = "https://github.com/datadog-labs/pup/releases/download/v${version}/pup_${version}_Darwin_arm64.tar.gz";
-      hash = "sha256-haoFH6DA33c5QImu3Sfa6DxS7xvtHYFN8Yw1O5hFMj4=";
+      hash = "sha256-xRU0XHjN/U96YqNVSS4xtSORwN+s94fYMJnjhcpkx5o=";
     };
 
     sourceRoot = ".";
