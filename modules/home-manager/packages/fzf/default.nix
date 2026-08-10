fed26219fc2bf3353610405992749c7db2c29895
Matthew Thomas <matthew.thomas@harvey.ai>
2026-08-10 19:18:46 -0400

    Move Nix stack to unstable

--- modules/home-manager/packages/fzf/default.nix
+++ modules/home-manager/packages/fzf/default.nix
@@ -16,23 +16,27 @@
         "--layout=reverse"
         "--info=inline"
       ];
-      fileWidgetCommand = "${pkgs.fd}/bin/fd --type f --hidden --follow --exclude .git";
-      fileWidgetOptions = [
-        "--height 100%"
-        "--layout=reverse"
-        "--info=inline"
-        "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range=:500 --wrap=auto {}'"
-        "--border=none"
-        "--preview-window=noborder"
-      ];
-      changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden --follow --exclude .git";
-      changeDirWidgetOptions = [
-        "--height 100%"
-        "--info=inline"
-        "--preview '${pkgs.eza}/bin/eza --tree --color=always --icons {}'"
-        "--border=none"
-        "--preview-window=noborder"
-      ];
+      fileWidget = {
+        command = "${pkgs.fd}/bin/fd --type f --hidden --follow --exclude .git";
+        options = [
+          "--height 100%"
+          "--layout=reverse"
+          "--info=inline"
+          "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range=:500 --wrap=auto {}'"
+          "--border=none"
+          "--preview-window=noborder"
+        ];
+      };
+      changeDirWidget = {
+        command = "${pkgs.fd}/bin/fd --type d --hidden --follow --exclude .git";
+        options = [
+          "--height 100%"
+          "--info=inline"
+          "--preview '${pkgs.eza}/bin/eza --tree --color=always --icons {}'"
+          "--border=none"
+          "--preview-window=noborder"
+        ];
+      };
     };
   };
 }
